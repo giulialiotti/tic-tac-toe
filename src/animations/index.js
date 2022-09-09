3bf4d9a435ba504f1f04d512cc92b971bfb66853
Giuliana Aliotti <giuliana.aliotti@hotmail.com>
2022-09-09 18:57:44 -0300

    Board Page: Added reveal animation

--- src/animations/index.js
+++ src/animations/index.js
@@ -1,8 +1,11 @@
 import { gsap } from "gsap";
 
+// Utils
+import { shuffleArray } from "../utils";
+
 export const revealHomePage = () => {
   let spans = gsap.utils.toArray(".headline-span");
-  let randomSpans = spans.sort(() => Math.random() - 0.5);
+  let randomSpans = shuffleArray(spans);
 
   let namePlayers = document.getElementById("name-players");
   let button = document.querySelector(".play-now-button");
@@ -21,6 +24,39 @@ export const revealHomePage = () => {
       stagger: 0.2,
     })
     .to(button, { y: 0, autoAlpha: 1 }, "-=0.4");
+
+  return () => {
+    tl.pause(0).kill(true);
+  };
+};
+
+export const revealBoardPage = () => {
+  let cells = gsap.utils.toArray(".cell");
+  let randomCells = shuffleArray(cells);
+
+  let scores = document.querySelector(".scores-wrapper");
+  let scoresElements = [...scores.children];
+
+  let tl = gsap.timeline({ defaults: { ease: "power2.out", duration: 0.6 } });
+
+  tl.to(randomCells, {
+    autoAlpha: 1,
+    scale: 1,
+    stagger: 0.2,
+  })
+    .to("#players-turn", {
+      y: 0,
+      autoAlpha: 1,
+    }, '-=0.2')
+    .to(
+      scoresElements,
+      {
+        y: 0,
+        autoAlpha: 1,
+        stagger: 0.2,
+      },
+      "-=0.4"
+    );
 
   return () => {
     tl.pause(0).kill(true);
--- src/pages/boardPage.js
+++ src/pages/boardPage.js
@@ -1,7 +1,18 @@
+import React from "react";
+
 // Local Components
 import { Board } from "../components";
 
+// Animations
+import { revealBoardPage } from "../animations";
+
 export const BoardPage = () => {
+  React.useEffect(() => {
+    const killAnimation = revealBoardPage();
+
+    return killAnimation;
+  }, []);
+
   return (
     <main className="board-main">
       <Board />
