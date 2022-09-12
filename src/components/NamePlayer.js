11c8b89b3c372debbb7ba8c79c3dac45a44f2a4a
Giuliana Aliotti <giuliana.aliotti@hotmail.com>
2022-09-12 09:15:15 -0300

    Small touch ups

--- src/components/NamePlayer.js
+++ src/components/NamePlayer.js
@@ -3,7 +3,7 @@ import React from "react";
 export const NamePlayer = ({ label }) => {
   return (
     <div className="name-player">
-      <label for="Name">{label}</label>
+      <label htmlFor="Name">{label}</label>
       <input type="text" id="Name" name="Name" placeholder="Enter a name" />
     </div>
   );
